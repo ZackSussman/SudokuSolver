1b920498cd8b271861739e0a4d8980d08be8f781
ZackSussman <65458956+ZackSussman@users.noreply.github.com>
2020-05-21 19:21:45 -0400

    fixed the past issue

--- canvas.py
+++ canvas.py
@@ -14,34 +14,31 @@ def passInfo():
     return 
 
 solveButton = Button(text = "Solve", command = passInfo )
+solveButton.pack()
+solveButton.place(x = 500, y = 10)
 gridEntries = []
 
-for x in range (10):
+for y in range (10):
     rowEntry = []
-    for y in range (10):
+    for x in range (10):
         rowEntry.append(StringVar())
-        textBox = Entry(root, width=int(((canvasWidth-initialSeparation)/10)/36), bg = "white", relief='flat', textvariable = rowEntry[y])
+        textBox = Entry(root, width=int(((canvasWidth-initialSeparation)/10)/36), bg = "white", relief='flat', textvariable = rowEntry[x])
         textBox.pack()
-        textBox.place(x = ((canvasWidth-initialSeparation)/10.0)*(x+1) + 13, y = ((canvasWidth-initialSeparation)/10.0)*(y+1) + 10 ) 
+        textBox.place(x = ((canvasWidth-initialSeparation)/10.0)*(x+1) + 13, y = ((canvasWidth-initialSeparation)/10.0)*(y+1) + 10) 
     gridEntries.append(rowEntry)
 
 
-    if (x % 3 == 0):
-        if (x == 0 or x == 9): 
-            line = canvas.create_line(((canvasWidth-initialSeparation)/10.0)*(x+1), (canvasWidth-initialSeparation)/10.0 - fixOuterBarsFactor, ((canvasWidth-initialSeparation)/10.0)*(x+1), canvasHeight - initialSeparation + fixOuterBarsFactor, width = 5)
-            line = canvas.create_line((canvasWidth-initialSeparation)/10.0 - fixOuterBarsFactor, ((canvasWidth-initialSeparation)/10.0)*(x+1),  canvasHeight - initialSeparation + fixOuterBarsFactor, ((canvasWidth-initialSeparation)/10.0)*(x+1), width = 5)
+    if (y % 3 == 0):
+        if (y == 0 or y == 9): 
+            line = canvas.create_line(((canvasWidth-initialSeparation)/10.0)*(y+1), (canvasWidth-initialSeparation)/10.0 - fixOuterBarsFactor, ((canvasWidth-initialSeparation)/10.0)*(y+1), canvasHeight - initialSeparation + fixOuterBarsFactor, width = 5)
+            line = canvas.create_line((canvasWidth-initialSeparation)/10.0 - fixOuterBarsFactor, ((canvasWidth-initialSeparation)/10.0)*(y+1),  canvasHeight - initialSeparation + fixOuterBarsFactor, ((canvasWidth-initialSeparation)/10.0)*(y+1), width = 5)
         else:
-            line = canvas.create_line(((canvasWidth-initialSeparation)/10.0)*(x+1), (canvasWidth-initialSeparation)/10.0, ((canvasWidth-initialSeparation)/10.0)*(x+1), canvasHeight - initialSeparation, width = 5)
-            line = canvas.create_line((canvasWidth-initialSeparation)/10.0, ((canvasWidth-initialSeparation)/10.0)*(x+1),  canvasHeight - initialSeparation, ((canvasWidth-initialSeparation)/10.0)*(x+1), width = 5)
+            line = canvas.create_line(((canvasWidth-initialSeparation)/10.0)*(y+1), (canvasWidth-initialSeparation)/10.0, ((canvasWidth-initialSeparation)/10.0)*(y+1), canvasHeight - initialSeparation, width = 5)
+            line = canvas.create_line((canvasWidth-initialSeparation)/10.0, ((canvasWidth-initialSeparation)/10.0)*(y+1),  canvasHeight - initialSeparation, ((canvasWidth-initialSeparation)/10.0)*(y+1), width = 5)
     else:
-        line = canvas.create_line(((canvasWidth-initialSeparation)/10.0)*(x+1), (canvasWidth-initialSeparation)/10.0, ((canvasWidth-initialSeparation)/10.0)*(x+1), canvasHeight - initialSeparation, width = 3)
-        line = canvas.create_line((canvasWidth-initialSeparation)/10.0, ((canvasWidth-initialSeparation)/10.0)*(x+1),  canvasHeight - initialSeparation, ((canvasWidth-initialSeparation)/10.0)*(x+1), width = 3)
+        line = canvas.create_line(((canvasWidth-initialSeparation)/10.0)*(y+1), (canvasWidth-initialSeparation)/10.0, ((canvasWidth-initialSeparation)/10.0)*(y+1), canvasHeight - initialSeparation, width = 3)
+        line = canvas.create_line((canvasWidth-initialSeparation)/10.0, ((canvasWidth-initialSeparation)/10.0)*(y+1),  canvasHeight - initialSeparation, ((canvasWidth-initialSeparation)/10.0)*(y+1), width = 3)
 
 canvas.pack()
 mainloop()
 
-
-for row in gridEntries: 
-    for entry in row:
-        print(entry.get())
-    print("\n")
